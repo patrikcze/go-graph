11cf3743e53c2097315f36e60a9bc75ab1851028
Nakladal, Patrik (Brno) CZE <patrik.nakladal@heidelbergcement.com>
2023-01-04 17:04:17 +0100

    feat(main.go): Changed chart package.

--- go.mod
+++ go.mod
@@ -3,13 +3,9 @@ module github.com/patrikcze/go-graph
 go 1.19
 
 require (
+	github.com/go-echarts/go-echarts/v2 v2.2.4
 	github.com/go-sql-driver/mysql v1.7.0
 	github.com/gorilla/mux v1.8.0
-	github.com/wcharczuk/go-chart v2.0.1+incompatible
 )
 
-require (
-	github.com/blend/go-sdk v1.20220411.3 // indirect
-	github.com/golang/freetype v0.0.0-20170609003504-e2365dfdc4a0 // indirect
-	golang.org/x/image v0.2.0 // indirect
-)
+require gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b // indirect
--- main.go
+++ main.go
@@ -1,18 +1,16 @@
 package main
 
 import (
-	"bytes"
+	"database/sql"
 	"log"
 	"net/http"
-	"strconv"
 	"time"
 
-	"github.com/gorilla/mux"
-	"github.com/wcharczuk/go-chart"
-
-	"database/sql"
-
 	_ "github.com/go-sql-driver/mysql"
+
+	"github.com/go-echarts/go-echarts/v2/charts"
+	"github.com/go-echarts/go-echarts/v2/opts"
+	"github.com/go-echarts/go-echarts/v2/types"
 )
 
 const (
@@ -22,11 +20,75 @@ const (
 )
 
 func main() {
-	r := mux.NewRouter()
+	/*r := mux.NewRouter()
 	r.HandleFunc("/graph", chartHandler)
 	http.ListenAndServe(":8080", r)
+	*/
+	http.HandleFunc("/", httpserver)
+	http.ListenAndServe(":8080", nil)
 }
 
+func httpserver(w http.ResponseWriter, _ *http.Request) {
+	// Reset Items
+	temperatures := make([]opts.LineData, 0)
+	var times = []time.Time{}
+	// Connect to the database
+	db, err := sql.Open("mysql", DB_USER+":"+DB_PASSWORD+"@/"+DB_NAME+"?parseTime=true")
+	if err != nil {
+		http.Error(w, "Error connecting to database: "+err.Error(), http.StatusInternalServerError)
+		return
+	}
+	defer db.Close()
+
+	// Query the data from the database
+	rows, err := db.Query("SELECT time, temperature FROM data")
+	if err != nil {
+		http.Error(w, "Error querying data: "+err.Error(), http.StatusInternalServerError)
+		return
+	}
+	defer rows.Close()
+
+	// Iterate through the data and add points to the series
+	for rows.Next() {
+		var t time.Time
+		var temp float64
+		err := rows.Scan(&t, &temp)
+		if err != nil {
+			log.Fatal(err)
+		}
+		// Convert the time value to a string
+		tString := t.Format("2006-01-02 15:04:05")
+
+		// Parse the time value from the database
+		t, err = time.Parse("2006-01-02 15:04:05", tString)
+		if err != nil {
+			log.Fatal(err)
+		}
+		// Append the time and temperature values to the chart data
+		times = append(times, t)
+		temperatures = append(temperatures, opts.LineData{Value: temp})
+
+	}
+
+	// Create a new line instance
+	line := charts.NewLine()
+	// set some global options like Title/Legend/ToolTip or anything else
+	line.SetGlobalOptions(
+		charts.WithInitializationOpts(opts.Initialization{Theme: types.ThemeWesteros}),
+		charts.WithTitleOpts(opts.Title{
+			Title:    "Graf teplot",
+			Subtitle: "Čárový graf teplot vykreslený.",
+		}))
+
+	// Put data into instance
+	line.SetXAxis(times).
+		AddSeries("Teploty", temperatures).
+		SetSeriesOptions(charts.WithLineChartOpts(opts.LineChart{Smooth: true}))
+	line.Render(w)
+
+}
+
+/*
 func chartHandler(c http.ResponseWriter, r *http.Request) {
 	// Connect to the database
 	db, err := sql.Open("mysql", DB_USER+":"+DB_PASSWORD+"@/"+DB_NAME+"?parseTime=true")
@@ -129,3 +191,4 @@ func chartHandler(c http.ResponseWriter, r *http.Request) {
 	}
 
 }
+*/
